743f1e7c0d52cdb8be7da6aac5b17749004c6cb4
Euan Jed Tabamo <79002463+u1tbmo@users.noreply.github.com>
2022-11-23 18:46:52 +0000

    Finish my-artm start about-section

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ import { Gradient } from './Gradient.js'
 
 // Create your instance
 const gradient = new Gradient()
+const gradient2 = new Gradient()
 
 // Call `initGradient` with the selector to your canvas
 gradient.initGradient('#gradient-canvas')
+gradient2.initGradient('#gradient-canvas-footer')
