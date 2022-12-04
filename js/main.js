10e18f0874be51ef992e5f00368f98c2789940d2
Euan Jed Tabamo <79002463+u1tbmo@users.noreply.github.com>
2022-12-04 23:50:28 +0800

    finally done (i think)
    aboutme.html and myart.html

--- js/main.js
+++ js/main.js
@@ -2,8 +2,12 @@ import { Gradient } from './Gradient.js'
 
 // Create your instance
 const gradient = new Gradient()
-const gradient2 = new Gradient()
+const gradientFooter = new Gradient()
+const gradientAboutMe = new Gradient()
+const gradientMyArt = new Gradient()
 
 // Call `initGradient` with the selector to your canvas
-gradient.initGradient('#gradient-canvas')
-gradient2.initGradient('#gradient-canvas-footer')
+gradient.initGradient('#gradient-canvas-index')
+gradientFooter.initGradient('#gradient-canvas-footer')
+gradientAboutMe.initGradient('#gradient-canvas-aboutme')
+gradientMyArt.initGradient('#gradient-canvas-myart')
